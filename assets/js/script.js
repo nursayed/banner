fa0a5d17aebe6440a18188cf502ee521678fd116
nur sayed <freelancernursayed@gmail.com>
2021-04-16 09:54:58 +0600

    Merge branch 'master' of https://github.com/nursayed/bannerad

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// document.getElementById('change-text').onclick = function () {
+//   document.getElementById(
+//     'text1'
+//   ).innerHTML = `<p1 style="padding-bottom: 30px;">Get a personal loan of upto $50,000 </p1> <br><br> <p2> with an attractive %3.1 <br> interest rate </p2> <br> <p3> <span class="green-text">And save upto $3000 </span> a <br> year! </p3> <br> <button>I'm interested!</button>`;
+// };
